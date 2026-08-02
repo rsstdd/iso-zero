0049aede6ac1ed792f37c67b3066fecde999ccd6
rsstdd <[rssmtdd@gmail.com](mailto:rssmtdd@gmail.com)>
2026-08-02 12:31:24 +0200

    tests: update testing boilerplate

--- e2e/smoke.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("the application boots and renders", async ({ page }) => {
-  await page.goto("/");
-  await expect(page.locator("body")).toBeVisible();
-});
